990b68b45bd57112b248c2619f6b404114a91eec
jayaprakash <jayaprakashjp187@gmail.com>
2023-06-22 20:10:26 +0530

    [creating linkedlist and insert the values]

---
+++ LinkedListDataStructurres/LinkedList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDataStructurres
+{
+    public class LinkedList
+    {
+        private Node head;
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+            if(head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+               Node current = head;
+                while(current.Next != null)
+                {
+                     current=current.Next;
+                }
+                current.Next = newNode;
+            }
+           
+
+        }
+        public void Display()
+        {
+           Node current = head;
+            while(current != null)
+            {
+                Console.WriteLine(current.Data);
+                current=current.Next;
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ LinkedListDataStructurres/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LinkedListDataStructurres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("choose what program u want ");
+            Console.WriteLine("1.creating linked list");
+            int choose=Convert.ToInt32(Console.ReadLine());
+            switch(choose)
+            {
+                case 1:
+                    Console.WriteLine("welcome to the creating of linked list program");
+                    LinkedList l = new LinkedList();
+                    l.Insert(56);
+                    l.Insert(30);
+                    l.Insert(70);
+                    l.Display();
+                    break;
+            }
+        }
+    }
+}
